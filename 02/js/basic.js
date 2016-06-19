e223ab411194ad3e02f8cff23d151dadf2e27750
kaseopea <kaseopea@gmail.com>
2016-06-19 11:33:35 +0300

    Basic template + basic operations + constructions

---
+++ 02/js/basic.js
@@ -0,0 +1,123 @@
+// ------------------------------------------------------------------------------------------------------------
+// Task 1
+//
+// Создайте 3 переменные   x = 6, y = 15, и z = 4:
+// Выполните и отобразите результат следующих операций для этих переменных:
+// · x += y - x++ * z ;
+// · z = -- x - y * 5 ;
+// · y /= x + 5 % z ;
+// · z = x++ + y * 5 ;
+// · x = y - x++ * z ;
+
+var x = 6;
+var y = 15;
+var z = 4;
+
+console.log("[ Задача 1 ]");
+console.log("Исходные данные: " + "x = " + x + ", y = " + y + ", z = " + z);
+
+
+//1
+console.log("\n1.\n");
+console.log("x += y - x++ * z");
+console.log("Результат: x = " + (x += y - x++ * z));
+
+//2
+console.log("\n2.\n");
+console.log("z = --x - y * 5");
+console.log("Результат: z = " + (z = --x - y * 5));
+
+//3
+console.log("\n3.\n");
+console.log("y /= x + 5 % z");
+console.log("Результат: y = " + (y /= x + 5 % z));
+
+//4
+console.log("\n4.\n");
+console.log("z = x++ + y * 5");
+console.log("Результат: z = " + (z = x++ + y * 5));
+
+//5
+console.log("\n5.\n");
+console.log("x = y - x++ * z");
+console.log("Результат: x = " + (x = y - x++ * z));
+
+
+console.log("\n\n");
+
+
+// ------------------------------------------------------------------------------------------------------------
+// Task 2
+//
+// Вычислите среднее арифметическое трех целочисленных значений и выведите его на экран.
+var x = 6;
+var y = 15;
+var z = 4;
+
+console.log("[ Задача 2 ]");
+console.log("Среднее арифметическое трех целочисленных значений");
+console.log("Исходные данные: " + "x = " + x + ", y = " + y + ", z = " + z);
+console.log( "Результат: " + (( x + y + z ) / 3) );
+
+console.log("\n\n");
+
+
+//Функция для подсчета среднего арифметического
+var arithmeticMean = function () {
+  var length = arguments.length;
+  var sum = 0;
+  if(length > 0 ) {
+    var args = [];
+    for ( var i = 0; i < length; i++) {
+        sum += arguments[i];
+    }
+    return sum / length;
+  } else {
+    return "Задайте операнды";
+  }
+}
+console.log("Подсчет через функцию: ");
+console.log("Среднее арифметическое: " + arithmeticMean(x, y, z));
+console.log("Среднее арифметическое: " + arithmeticMean(32, 8, 1, 10, 34));
+
+console.log("\n\n");
+
+// ------------------------------------------------------------------------------------------------------------
+// Task 3
+//
+// Напишите программу расчета объема - V и площади поверхности - S цилиндра.
+// Объем V цилиндра радиусом - r и высотой – h, вычисляется по формуле: V = πr 2 h.
+// Площадь S поверхности цилиндра вычисляется по формуле: S = 2π rh + 2π r 2 = 2π r (r+ h).
+// Результаты расчетов выведите на экран.
+
+var cylinderVolume = function (radius, height) {
+  var r = radius || 0;
+  var h = height || 0;
+  if ((r > 0) && (h > 0)) {
+    return Math.PI * Math.pow(r, 2) * h;
+  } else {
+    return "Задайте корректные данные";
+  }
+
+}
+
+var cylinderSurfaceArea = function (radius, height) {
+  var r = radius || 0;
+  var h = height || 0;
+  if ((r > 0) && (h > 0)) {
+    return 2 * Math.PI * r * (r + h) ;
+  } else {
+    return "Задайте корректные данные";
+  }
+}
+
+//Tests
+var r = 15;
+var h = 24;
+
+console.log("[ Задача 3 ]");
+console.log("Исходные данные:");
+console.log("Радиус = " + r);
+console.log("Высота = " + h);
+console.log("Объем цилиндра: " + cylinderVolume(r,h));
+console.log("Площадь поверхности: " + cylinderSurfaceArea(r,h));
---
+++ 02/js/constructions.js
@@ -0,0 +1,158 @@
+// ------------------------------------------------------------------------------------------------------------
+// task 1
+// Дано два числа A и B где (A<B).
+// Выведите на экран суму всех чисел расположенных в числовом промежутке от А до В.
+// Выведите на экран все нечетные значения, расположенные в числовом промежутке от А до В.
+
+var sumRange = function (start, end) {
+  var sum = 0;
+  for ( var i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+var oddNumbersRange = function (start, end) {
+  var numbers = [];
+  var first = ( start % 2 == 0 ) ? (start + 1) : (start);
+  for ( var i = first; i <= end; i += 2) {
+    numbers.push(i);
+  }
+  return numbers.toString();
+}
+
+console.log("[ Задача 1 ]");
+console.log("Сумма чисeл в диапазоне [10..20] - " + sumRange(10,20));
+console.log("Нечетные числа в диапазоне [10..20] - " + oddNumbersRange(10,20));
+
+console.log("\n\n");
+
+// ------------------------------------------------------------------------------------------------------------
+// Task 2
+// Напишите программу, которая будет рассчитывать и выводить на экран количество возможных вариантов доставки товара. Для решения задачи, используйте факториал N!, рассчитываемый с помощью цикла do-while .
+//
+var possibleDeliveryOptions = function (number) {
+  var factorial = 1;
+  var counter = number;
+  do {
+      if (counter == 0) {
+          break;
+      }
+      factorial *= counter;
+      counter -= 1;
+  } while (counter > 0);
+
+  return factorial;
+}
+
+console.log("[ Задача 2 ]");
+var goods = 7;
+console.log("Исходные данные: " + "Количество товаров = " + goods);
+console.log("Количество возможных вариантов доставки товара: " + possibleDeliveryOptions(goods));
+console.log("\n\n");
+
+
+// ------------------------------------------------------------------------------------------------------------
+// Task 3
+// Используя циклы нарисуйте в браузере с помощью пробелов (&nbsp) и звездочек (*):
+// · Прямоугольник
+// · Прямоугольный треугольник
+// · Равносторонний треугольник
+// · Ромб.
+
+// Функция рисования прямоугольника
+var drawRectangle = function (width, height) {
+  var output = '';
+  for ( var i = 0; i < width; i++) {
+    for ( var j = 0; j < height; j++) {
+      if ( (i == 0) || (i == width - 1) || (j == 0) || (j == height - 1)) {
+        output += "*";
+      } else {
+        output += " ";
+      }
+    }
+    output += "\n";
+  }
+  return output;
+}
+
+// Функция рисования прямоугольного треугольника
+var triangleRight = function (size) {
+  var output = '';
+  for ( var i = 0; i < size; i++) {
+    for ( var j = 0; j < i + 1; j++ ) {
+      if ((j == 0) || (j == i) || (i == size - 1)) {
+        output += "*";
+      } else {
+        output += " ";
+      }
+    }
+    output += "\n";
+  }
+  return output;
+}
+
+// Функция рисования равностороннего треугольника
+var triangleEqual = function (size) {
+  var output = '';
+  for( var i = (size / 2) + 1; i <= size; i++ ) {
+    for( var j = 0; j <= i; j++) {
+      if ( j < size - i + 1) {
+        output += " ";
+      } else {
+        output += "*";
+      }
+    }
+    output += "\n";
+  }
+  return output;
+}
+
+
+// Функция рисования ромба
+var rhombus = function (size) {
+  var output = '';
+  var middle = Math.floor(size / 2);
+  for( var i = -middle; i <= middle; i++) {
+    var stars = size - (Math.abs(i)*2);
+    output += renderLine(stars, size);
+  }
+
+  function renderLine ( stars, size ) {
+    var sides = Math.floor((size - stars) / 2);
+    var output = '';
+
+    output += renderSide(sides);
+    output += renderStar(stars);
+    output += renderSide(sides);
+    // console.log(output);
+    return output + "\n";
+
+    function renderStar(stars) {
+      var output = '';
+      for ( var i = 0; i < stars; i++ ) {
+        output += "*";
+      }
+      return output;
+    }
+
+    function renderSide (size) {
+      var output = '';
+      for ( var i = 0; i < size; i++ ) {
+        output += " ";
+      }
+      return output;
+    }
+  }
+  return output;
+}
+console.log("[ Задача 3 ]");
+console.log(drawRectangle(3,30));
+console.log(triangleRight(10));
+console.log(triangleEqual(11));
+console.log(rhombus(3)); //check for minimal value
+
+// todo
+// check for minimal value triangleEqual and rhombus;
+// optimize output
+// try to make function where right triangle has different sizes
